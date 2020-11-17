1f9d96e19ad6100ae8877554d60cc90c3a640140
Simon Sprankel <simon@customgento.com>
2020-11-17 17:08:53 +0100

    Refactor import, SUP-4157

--- src/app/code/community/EasyTranslate/Connector/Model/Content/Importer/AbstractImporter.php
+++ src/app/code/community/EasyTranslate/Connector/Model/Content/Importer/AbstractImporter.php
@@ -4,11 +4,6 @@ declare(strict_types=1);
 
 abstract class EasyTranslate_Connector_Model_Content_Importer_AbstractImporter
 {
-    /**
-     * @var array
-     */
-    protected $_objects;
-
     public function import(array $data, int $sourceStoreId, array $targetStoreIds): void
     {
         $lastId     = null;
@@ -17,32 +12,22 @@ abstract class EasyTranslate_Connector_Model_Content_Importer_AbstractImporter
             $delimiter = EasyTranslate_Connector_Model_Content_Generator_AbstractGenerator::KEY_SEPARATOR;
             [$entityCode, $currentId, $attributeCode] = explode($delimiter, $key);
             if ($lastId !== null && $currentId !== $lastId) {
-                $this->_createObjects($lastId, $attributes, $sourceStoreId, $targetStoreIds);
+                $this->_importObject($lastId, $attributes, $sourceStoreId, $targetStoreIds);
                 $attributes = [];
             }
             $attributes[$attributeCode] = $content;
             $lastId                     = $currentId;
         }
-        // make sure to import the last entity as well
+        // make sure to import the last object as well
         if ($lastId !== null) {
-            $this->_createObjects($lastId, $attributes, $sourceStoreId, $targetStoreIds);
+            $this->_importObject($lastId, $attributes, $sourceStoreId, $targetStoreIds);
         }
-        $this->_bulkSave();
     }
 
-    abstract protected function _createObjects(
+    abstract protected function _importObject(
         string $id,
         array $attributes,
         int $sourceStoreId,
         array $targetStoreIds
     ): void;
-
-    protected function _bulkSave(): void
-    {
-        $transaction = Mage::getModel('core/resource_transaction');
-        foreach ($this->_objects as $object) {
-            $transaction->addObject($object);
-        }
-        $transaction->save();
-    }
 }
