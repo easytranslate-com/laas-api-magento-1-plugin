ac8dbc681ad37b2b206d81a7b6dccd6149cc17a3
Simon Sprankel <simon@customgento.com>
2020-12-09 11:01:27 +0100

    Throw exception if API returns invalid response

--- src/lib/EasyTranslate/Api/Response/PriceApprovalResponse.php
+++ src/lib/EasyTranslate/Api/Response/PriceApprovalResponse.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace EasyTranslate\Api\Response;
 
+use EasyTranslate\Api\ApiException;
+
 class PriceApprovalResponse extends AbstractResponse
 {
     /**
@@ -23,15 +25,16 @@ class PriceApprovalResponse extends AbstractResponse
 
     public function mapFields(array $data): void
     {
-        if (isset($data['data']['type'], $data['data']['id']) && $data['data']['type'] === 'project') {
-            $this->projectId = $data['data']['id'];
-            if (isset($data['data']['attributes']['price'])) {
-                $priceInCents   = (int)$data['data']['attributes']['price']['amount'];
-                $this->price    = (float)$priceInCents / 100;
-                $this->currency = $data['data']['attributes']['price']['currency'];
-            }
-        }
         parent::mapFields($data);
+        if (!isset($data['data']['type'], $data['data']['id']) || $data['data']['type'] !== 'project') {
+            throw new ApiException(sprintf('Invalid response data in response class %s', self::class));
+        }
+        $this->projectId = $data['data']['id'];
+        if (isset($data['data']['attributes']['price'])) {
+            $priceInCents   = (int)$data['data']['attributes']['price']['amount'];
+            $this->price    = (float)$priceInCents / 100;
+            $this->currency = $data['data']['attributes']['price']['currency'];
+        }
     }
 
     public function getProjectId(): string
